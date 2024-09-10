18898776c58025a2f3096349eab0ff0bdf9a0d42
NeosGao <601559101@qq.com>
2024-09-10 17:28:15 +0800

    feat: home done

---
+++ src/view/home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="w-full h-[880px] bg-cover bg-center bg-[url('/src/assets/img/home/home.png')]"
+  ></div>
+  <div
+    class="bg-[#f8f7f8] w-full h-[200px] flex flex-col items-center justify-center"
+  >
+    <div class="flex flex-row items-center justify-center">
+      <div class="flex items-center justify-center p-[50px] px-[100px]">
+        <img src="../assets/img/home/home1.png" alt="" />
+        <div class="ml-5">
+          <img src="../assets/img/home/home2.png" alt="" class="mt-[25px]" />
+          <img src="../assets/img/home/home3.png" class="mt-[10px]" alt="" />
+        </div>
+      </div>
+      <div class="flex items-center justify-center p-[50px] px-[100px]">
+        <img src="../assets/img/home/home4.png" alt="" />
+        <div class="ml-5">
+          <img src="../assets/img/home/home5.png" alt="" class="mt-[25px]" />
+          <img src="../assets/img/home/home6.png" class="mt-[10px]" alt="" />
+        </div>
+      </div>
+      <div class="flex items-center justify-center p-[50px] px-[100px]">
+        <img src="../assets/img/home/home7.png" alt="" />
+        <div class="ml-5">
+          <img src="../assets/img/home/home8.png" alt="" class="mt-[25px]" />
+          <img src="../assets/img/home/home9.png" class="mt-[10px]" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-full px-[500px] py-[50px] incenter">
+    <div>
+      <img src="../assets/img/home/home10.png" alt="" />
+      <div class="bg-[#57b8a8] incenter py-[40px]">
+        <img src="../assets/img/home/home11.png" alt="" />
+      </div>
+    </div>
+    <div class="ml-[70px]">
+      <img src="../assets/img/home/home12.png" alt="" />
+      <div class="bg-[#57b8a8] incenter py-[40px]">
+        <img src="../assets/img/home/home13.png" alt="" />
+      </div>
+    </div>
+  </div>
+  <div
+    class="bg-[#f8f7f8] w-full h-[200px] flex flex-col items-center justify-center"
+  >
+    <div class="flex items-center justify-center">
+      <div>
+        <img src="../assets/img/home/home14.png" alt="" />
+        <img src="../assets/img/home/home15.png" class="mt-[15px]" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
